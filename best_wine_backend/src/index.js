3d3dcfb8b658d7dc6d96ea9f31be8615be56af78
mgautam2 <mgautam2@illinois.edu>
2021-04-30 16:48:28 -0500

    Added heroku.yaml

--- best_wine_backend/src/index.js
+++ best_wine_backend/src/index.js
@@ -1,7 +1,7 @@
 const app = require("./app.js");
 
-const PORT = process.env.PORT || 4000
-
+const PORT = process.env.SERVER_PORT || 4000
+console.log(process.env)
 app.listen( PORT, () => { 
   console.log("Server started listening on port:", PORT);
 });
